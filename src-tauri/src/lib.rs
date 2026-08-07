f5ca2d47d6a038b0f6b039f2e96912a6861be55c
CosminSebastian2025 <208377106+CosminSebastian2025@users.noreply.github.com>
2026-08-07 13:57:06 +0200

    Initial commit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
             .plugin(tauri_plugin_updater::Builder::new().build())
+            .plugin(tauri_plugin_opener::init())
             .invoke_handler(tauri::generate_handler![])
             .run(tauri::generate_context!())
             .expect("error while running tauri application");
